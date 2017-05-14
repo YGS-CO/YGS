3311502de4cc2ed108cf6e2632f7058acac1d78b
David <dzarezad@gmail.com>
2017-05-14 14:44:19 +0430

    Update danestaniha.lua

--- plugins/danestaniha.lua
+++ plugins/danestaniha.lua
@@ -1,8 +1,7 @@
---نوشته شده توسط شایان احمدی
 local database = 'http://umbrella.shayan-soft.ir/txt/'
 local function run(msg)
   local res = http.request(database.."danestani.db")
-  if string.match(res, '@UmbrellaTeam') then res = string.gsub(res, '@UmbrellaTeam', "")
+  if string.match(res, '@YGSCO') then res = string.gsub(res, '@YGSCO', "")
  end
   local danestani = res:split(",")
   return danestani[math.random(#danestani)]
@@ -17,4 +16,3 @@ return {
   },
   run = run
 }
---نوشته شده توسط شایان احمدی
